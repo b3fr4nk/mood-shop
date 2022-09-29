2859d2931f7dce3c43bd6fe72b23b4172f6d32f7
Chris Wood <wchristopherwood912@gmail.com>
2022-09-29 09:51:23 -0700

    added add to cart buttons

--- script.js
+++ script.js
@@ -41,22 +41,77 @@ for(let i = 0; i < data.length; i++){
 }
 
 let cart = []
+let items = []
 
-function addItem(name, price){
-    const item = {name: name, price: price, qty: 1}
-    cart.push(item)
+const itemList = document.getElementById('item-list')
+const all_items_button = Array.from(document.querySelectorAll("button"))
+
+function showItems() {
+    itemList.innerHTML = "" //reset the list
+    //adds all items to the list
+    for (let i = 0; i < cart.length; i++) {
+        const name = cart[i].name
+        const price = cart[i].price
+        const qty = cart[i].qty
+
+        //create the new li and add the content
+        const itemElem = document.createElement("li")
+        itemElem.innerHTML = `<p>${name}: ${price} X${qty}</p>`
+
+        //append new li to the list
+        itemList.appendChild(itemElem)
+    }
+
+    //display total price
+    const priceElem = document.createElement("li")
+    priceElem.innerHTML = `total: ${getTotalPrice()}`
+    priceElem.className = "total-price"
+
+    itemList.appendChild(priceElem)
 }
 
-function showItems(){
-    console.log(cart)
+
+
+function contains(list, item){
+    for(let i = 0; i < list.length; i++){
+        if(item == list[i])
+            return i
+    }
+    return null
+}
+
+function addItem(name, price){
+    let index = contains(items, name)
+    if(index != null){
+        cart[index].qty++
+    }
+    else {
+        const item = { name: name, price: price, qty: 1 }
+        cart.push(item)
+
+        items.push(item.name) 
+    }   
+    //auto update the visible cart every time a new item is added
+    showItems()
 }
 
 function getTotalPrice(){
     let total = 0
     for(let i = 0; i < cart.length; i++){
-        total += cart[i].price
+        total += cart[i].price * cart[i].qty
     }
     return total
 }
 
-showItems()
+function removeItem(index=cart.length-1){
+    cart.splice(index, 1)
+}
+
+function removeAll(){
+    cart = []
+}
+
+all_items_button.forEach(elt => elt.addEventListener('click', () => {
+    addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
+    showItems()
+}))
